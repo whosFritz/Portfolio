b9f1235832333d9b61043efd67f8c1e74f48e7a7
whosfritz <fritzschubert21@outlook.de>
2023-11-15 10:18:14 +0100

    fixed bug, that I caused myself

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.onload = function () {
   // Make a GET request to the Spotify API
-
-  let headersList = {
-    Accept: "*/*",
-  };
 
   fetch("https://spotify.whosfritz.de/spotifywhosfritz", {
     method: "GET",
-    headers: headersList,
   })
     .then((response) => response.json())
     .then((data) => {
       // Update the src attribute of the iframe with the received favSongID
       const spotifyIframe = document.getElementById("spotifyIframe");
-      spotifyIframe.src = `https://open.spotify.com/embed/track/${data.favSongID}?utm_source=generator`;
+      spotifyIframe.src = `https://open.spotify.com/embed/track/${data.fav_Song_ID}?utm_source=generator`;
+      console.log(data);
     })
     .catch((error) => console.error("Error fetching data:", error));
-});
+};
 
 // Function to set a cookie
 function setCookie(name, value, days) {
